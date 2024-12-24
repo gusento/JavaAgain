f6b1a57042edad0a8dc68aaf16e7a9304771ac36
gusento <sebastian.segura1310@gmail.com>
2024-12-24 16:25:35 -0500

    lesson1

--- lesson1/lesson1/src/Estudiante.java
+++ lesson1/lesson1/src/Estudiante.java
@@ -1,2 +1,22 @@
 public class Estudiante {
+    public String name;
+    public String lastName;
+    public String code;
+    public String course;
+    public String craest;
+    public Boolean exchange;
+    public String entryYear;
+
+    public void saludaAmigo(String nombre){
+        System.out.println("Soy "+this.name+" y saludo a: "+nombre);
+    }
+
+    public void ciclosRestantes(){
+        System.out.println("");
+    }
+    public void cambiarCarrera(String newCourse){
+        course = newCourse;
+    }
+
 }
+
--- lesson1/lesson1/src/Main.java
+++ lesson1/lesson1/src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world from java!");
+        Estudiante estudiante1 = new Estudiante();
+        estudiante1.name="Sebastian";
+        estudiante1.lastName="Segura";
+        estudiante1.course="Computer Science";
+        estudiante1.code="20203368";
+        //ingreso por teclado Scanner
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingresa algo: ");
+        String nombre = scanner.nextLine();
+        scanner.close();
+        
+        System.out.println("lo que ingrese fue: " + nombre);
     }
 }
